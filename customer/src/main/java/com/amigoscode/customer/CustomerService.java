3d08bd5c57791c0b73a83a3f7eb85dd648f2bb3c
pradeshi <pradeep.shinde@amdocs.com>
2022-05-28 11:04:43 +0530

    Customer MS

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,19 @@
+package com.amigoscode.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(CustomerRepository customerRepository) {
+    public void registerCustomer(CustomerRegistrationRequest request){
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        // todo : Check if email Valid
+        // todo: Check if email not taken
+        // store customer in db
+        customerRepository.save(customer);
+    }
+}
