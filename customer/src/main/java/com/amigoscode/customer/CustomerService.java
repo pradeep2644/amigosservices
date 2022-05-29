df1bc95480f6ca4826fb064f759711c2cc1ba3ac
pradeshi <pradeep.shinde@amdocs.com>
2022-05-29 09:28:45 +0530

    Faurd MS update

--- customer/src/main/java/com/amigoscode/customer/CustomerService.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -1,9 +1,17 @@
 package com.amigoscode.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService{
+
+    private final CustomerRepository customerRepository;
+
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request){
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -13,7 +21,17 @@ public record CustomerService(CustomerRepository customerRepository) {
 
         // todo : Check if email Valid
         // todo: Check if email not taken
-        // store customer in db
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        // check if Fraudster
+      FraudCheckResponse fraudCheckResponse = restTemplate.
+              getForObject(
+                      "http://FRAUD/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId());
+
+      if(fraudCheckResponse.isFraudSter())
+          throw  new IllegalStateException("fraudster");
+        // todo : send Notification
     }
 }
